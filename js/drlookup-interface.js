d577c2ff986d7faf2c11e3454ab0e56f73a0c367
Bably Kumari <bably@Bablys-MacBook-Pro.local>
2017-10-20 15:24:48 -0700

    add interface.js file

--- js/drlookup-interface.js
+++ js/drlookup-interface.js
@@ -1,12 +1,17 @@
 import { DrLookup } from './../js/drlookup.js'
 
 $(document).ready(function(){
-  $('#drlookup').submit(function() {
-    let location = $('#location').val();
-    let distance = $('#distance').val();
-    $('#location').val("");
-    $('#distance').val("");
-    let bike = new Bike();
-    bike.listBikeByLocation(location, distance);
+  $('#drlookup').submit(function(e) {
+    e.preventDefault();
+    let state = $('#state').val();
+    state = state.toLowerCase();
+    let city = $('#city').val();
+    city = city.toLowerCase();
+    let condition = $('#condition').val();
+    $('#state').val("");
+     $('#city').val("");
+     $('#condition').val("");
+    let lookup = new DrLookup();
+    lookup.listDoctors(state, city, condition);
   })
 })
